2ef8211151263b60a8587036e105e3bc4391cfb0
Melissa Lineaweaver <MelissaHastie16@gmail.com>
2022-03-01 16:25:53 -0500

    updated registration route

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
       component: HomeComponent,
     },
     {
-      path: 'registration',
+      path: 'register',
       component: RegistrationComponent,
     },
   ];
