3b0c37c488ad1fa712610704b9dd0fb9bb39eae2
Melissa Lineaweaver <MelissaHastie16@gmail.com>
2022-03-05 16:32:32 -0700

    reservation and charge forms

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -6,6 +6,7 @@ import { FormArray } from "@angular/forms";
 @Component({
     selector: 'app-registration',
     templateUrl: './registration.component.html',
+    styleUrls: ['./registration.component.scss']
     
   })
   export class RegistrationComponent {}
